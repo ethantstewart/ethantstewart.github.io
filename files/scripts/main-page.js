d4370a1b57f5597e7a129383d16c7bf1c1aa7468
Ethan <ethantstewart0@gmail.com>
2025-05-19 18:42:51 -0400

    Implemented a "dynamically loaded" navigation system. CORS will prevent cross-site HTTP/HTTPS requests, but it would work in theory.

--- files/scripts/main-page.js
+++ files/scripts/main-page.js
@@ -1,14 +1,14 @@
 // Preload page data (?)
 
-function internalNavigation(page) {
-    // Change the URL without navigating
-    window.history.pushState(page, "", "/" + page);
-    loadContent(page);
+function initialFullLoad(page) {
+    loadCommonElements();
+    contentNavigation(page);   
 }
 
 function contentNavigation(page) {
     changeActiveNavButton(page);
-    internalNavigation(page);
+    window.history.pushState(page, "", "/" + page);
+    loadContent(page);
 }
 
 function changeActiveNavButton(page) {
@@ -24,11 +24,49 @@ function changeActiveNavButton(page) {
 }
 
 function loadContent(page) {
+    var pageJSON = loadPageJSON(page);
     var loadRequest = new XMLHttpRequest();
     loadRequest.open("GET", window.location.origin + "/files/body-pages/" + page + ".html", false);
     loadRequest.send();
     // This is not an ideal solution; it simply copies the html file text and pastes it into page-body's innerhtml
     document.getElementById("page-body").innerHTML = loadRequest.response;
+
+    var cardHolder = document.getElementsByClassName("content-card-container")[0];
+
+    if (cardHolder) {
+        pageJSON = pageJSON.cards;
+        for (card in pageJSON) {
+            cardHolder.appendChild(createCard(pageJSON[card]));
+        }
+    }
+}
+
+function createCard(card) {
+    var content_card = document.createElement("div");
+    content_card.classList.add("content-card");
+    if (card.image_src) {
+        card_img = document.createElement("img");
+        card_img.src = window.location.origin + "/files/images/" + card.image_src;
+        content_card.appendChild(card_img);
+    }
+    if (card.description) {
+        card_desc = document.createElement("p");
+        card_desc.innerText = card.description;
+        content_card.appendChild(card_desc);
+    }
+
+    return content_card;
+}
+
+function loadPageJSON(page) {
+    var loadRequest = new XMLHttpRequest();
+    loadRequest.open("GET", window.location.origin + "/files/JSON/" + page + ".JSON", false);
+    loadRequest.send();
+    if (loadRequest.status != 200 || !loadRequest.response) {
+        return;
+    }
+    var pageJSON = JSON.parse(loadRequest.response);
+    return pageJSON;
 }
 
 // Navigate when the back or forward button is used
@@ -37,3 +75,24 @@ window.addEventListener("popstate", (e) => {
         contentNavigation(e.state);
     }
 });
+
+function loadHeader(header) {
+    var loadRequest = new XMLHttpRequest();
+    loadRequest.open("GET", window.location.origin + "/files/body-pages/header.html", false);
+    loadRequest.send();
+    // This is not an ideal solution; it simply copies the html file text and pastes it into header's innerhtml
+    header.innerHTML = loadRequest.response;
+}
+
+function loadFooter(footer) {
+    var loadRequest = new XMLHttpRequest();
+    loadRequest.open("GET", window.location.origin + "/files/body-pages/footer.html", false);
+    loadRequest.send();
+    // This is not an ideal solution; it simply copies the html file text and pastes it into footer's innerhtml
+    footer.innerHTML = loadRequest.response;
+}
+
+function loadCommonElements() {
+    loadHeader(document.getElementById("page-header"));
+    loadFooter(document.getElementById("page-footer"));
+}
